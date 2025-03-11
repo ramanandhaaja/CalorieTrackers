d0ebb02f648f7d6fae9f0113515cda52692c9a84
ramanandhaaja <thisisnandha@gmail.com>
2025-03-11 19:40:25 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,8 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './src'),
-    };
-    return config;
-  },
 }
 
 export default withPayload(nextConfig)
